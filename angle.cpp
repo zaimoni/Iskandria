305edd0dc2730a9129fbd896370bfc419db85224
Kenneth Boyd <zaimoni@zaimoni.com>
2017-07-05 18:18:45 -0500

    prototype trigonometry wrapper for angle class

--- angle.cpp
+++ angle.cpp
@@ -53,6 +53,48 @@ void zaimoni::circle::angle::_to_standard_form()
 	(_theta*=225)/=8;
 }
 
+#if 0
+void zaimoni::circle::angle::_radian_sincos(boost::numeric::interval<double> radians, boost::numeric::interval<double>& _sin, boost::numeric::interval<double>& _cos)
+{
+	_sin = sin(radians);	// Boost fails on these two lines.  Simulate with Taylor series?
+	_cos = cos(radians);
+//	\todo post-processing
+}
+
+// x is in the internal representation
+void zaimoni::circle::angle::_sincos(boost::numeric::interval<double> x, boost::numeric::interval<double>& _sin, boost::numeric::interval<double>& _cos)
+{
+//	\todo preprocessing
+	if (5062.5 >= x.upper() && 2531.25<=x.lower())
+		{	// quadrant II -> quadrant IV
+		_sincos(boost::numeric::interval<double>(x.lower()-5062.5,x.upper()-5062.5),_sin,_cos);
+		_sin.assign(-_sin.upper(),-_sin.lower());
+		_cos.assign(-_cos.upper(),-_cos.lower());
+		return;
+		}
+	if (-5062.5 <= x.lower() && -2531.25>=x.upper())
+		{	// quadrant III -> quadrant I
+		_sincos(boost::numeric::interval<double>(x.lower()+5062.5,x.upper()+5062.5),_sin,_cos);
+		_sin.assign(-_sin.upper(),-_sin.lower());
+		_cos.assign(-_cos.upper(),-_cos.lower());
+		return;
+		}
+//	go to radians
+	_radian_sincos(((x*2.0)*M_PI)/1125.0, _sin, _cos);
+}
+
+void zaimoni::circle::angle::sincos(boost::numeric::interval<double>& _sin, boost::numeric::interval<double>& _cos)
+{
+	if (is_whole_circle())
+		{
+		_sin.assign(-1.0,1.0);
+		_cos.assign(-1.0,1.0);
+		return;
+		}
+	_sincos(_theta,_sin,_cos);
+}
+#endif
+
 #ifdef TEST_APP
 // fast compile test
 // g++ -std=c++11 -otest.exe -Os -DTEST_APP angle.cpp
