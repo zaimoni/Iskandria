5ed4b8c9a804664d614b6b17c326a283d3a7efdb
Kenneth Boyd <zaimoni@zaimoni.com>
2018-07-24 23:36:11 -0500

    upgrade Pure.C\stdio.log to current practice

--- Zaimoni.STL/Pure.C/stdio.log/fatal.c
+++ Zaimoni.STL/Pure.C/stdio.log/fatal.c
@@ -1,15 +1,26 @@
 /* fatal.c */
 /* implements Logging.h for C stdout/stderr */
-/* (C)2009 Kenneth Boyd, license: MIT.txt */
+/* (C)2009,2018 Kenneth Boyd, license: MIT.txt */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#ifdef GUI_FATAL
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+#endif
 
 void _fatal(const char* const B)
 {
 	fwrite(B,strlen(B),1,stderr);
 	fwrite("\n",1,1,stderr);
+#ifdef GUI_FATAL
+#ifdef _WIN32
+	MessageBox(NULL,B,GUI_FATAL,MB_ICONSTOP | MB_OK | MB_SYSTEMMODAL);
+#endif
+#endif
 	exit(EXIT_FAILURE);
 }
 
--- Zaimoni.STL/Pure.C/stdio.log/fatal_code.c
+++ Zaimoni.STL/Pure.C/stdio.log/fatal_code.c
@@ -1,15 +1,26 @@
 /* fatal_code.c */
 /* implements Logging.h for C stdout/stderr */
-/* (C)2009 Kenneth Boyd, license: MIT.txt */
+/* (C)2009,2018 Kenneth Boyd, license: MIT.txt */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#ifdef GUI_FATAL
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+#endif
 
 void _fatal_code(const char* const B,int exit_code)
 {
 	fwrite(B,strlen(B),1,stderr);
 	fwrite("\n",1,1,stderr);
+#ifdef GUI_FATAL
+#ifdef _WIN32
+	MessageBox(NULL,B,GUI_FATAL,MB_ICONSTOP | MB_OK | MB_SYSTEMMODAL);
+#endif
+#endif
 	exit(exit_code);
 }
 
