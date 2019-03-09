233252f00fbe4cbba3ffe26ff58cf76413b347d0
Kenneth Boyd <zaimoni@zaimoni.com>
2019-03-08 22:53:42 -0600

    actually wire in the Minkoski space model

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include "game_manager.hpp"
 #include "gridspace.hpp"
+#include "minkowski.hpp"
 #include "agent.hpp"
 #include "craft.hpp"
 
@@ -9,6 +10,7 @@ int main(int argc, char* argv[])
 {
 	// Order of world registration of game objects, is their order in the savefile.
 	iskandria::grid::cartesian_2d::world_setup();
+	iskandria::minkowski::world_setup();
 
 	iskandria::agent::world_setup();
 	iskandria::craft::world_setup();
