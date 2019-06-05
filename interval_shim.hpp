d1e86da18acab8fa477ea1257a1ab99372d60208
zaimoni <none@none>
2019-06-05 03:29:13 -0500

    macro ISK_INTERVAL

--- interval_shim.hpp
+++ interval_shim.hpp
@@ -5,29 +5,33 @@
 
 #ifdef CONSTANTS_ISK_INTERVAL
 #include "interval.hpp"
+#define ISK_INTERVAL zaimoni::math::interval
 #else
 #include <boost/numeric/interval.hpp>
 #include "Zaimoni.STL/augment.STL/type_traits"
+#define ISK_INTERVAL boost::numeric::interval
 #endif
 
 struct interval_shim
 {
-#ifdef CONSTANTS_ISK_INTERVAL
-	typedef zaimoni::math::interval<double> interval;
-#else
-	typedef boost::numeric::interval<double> interval;
-#endif
-
+	typedef ISK_INTERVAL<double> interval;
 	static const interval pi;	// define is a point estimate
+};
+
+template<class T>
+struct interval_shim_template
+{
+	typedef ISK_INTERVAL<T> interval;
 };
 
 namespace zaimoni {
 
 template<>
-struct definitely<interval_shim::interval>
+struct definitely<typename interval_shim::interval>
 {
-	static bool equal(typename const_param<interval_shim::interval>::type lhs, typename const_param<interval_shim::interval>::type rhs) { return rhs.upper()==rhs.lower() && lhs == rhs.upper(); }
-	static bool unequal(typename const_param<interval_shim::interval>::type lhs, typename const_param<interval_shim::interval>::type rhs) { return lhs.upper()<rhs.lower() || rhs.upper()<lhs.lower(); }
+	typedef typename interval_shim::interval interval;
+	static bool equal(typename const_param<interval>::type lhs, typename const_param<interval>::type rhs) { return rhs.upper()==rhs.lower() && lhs == rhs.upper(); }
+	static bool unequal(typename const_param<interval>::type lhs, typename const_param<interval>::type rhs) { return lhs.upper()<rhs.lower() || rhs.upper()<lhs.lower(); }
 };
 
 }	// namespace zaimoni
