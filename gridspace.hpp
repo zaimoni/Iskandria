e78f66fee97a1d78171176a5af1876005e5271cf
zaimoni <tharichizi@gmail.com>
2019-11-16 19:51:05 -0600

    coordinate conversion support for cartesian grid spaces

--- gridspace.hpp
+++ gridspace.hpp
@@ -26,12 +26,17 @@ private:
 //	std::vector<std::weak_ptr<agent> > _agents;
 //	std::vector<std::weak_ptr<craft> > _crafts;
 	zaimoni::gdi::box<coord_type> _domain;
+	size_t _size;	// cache
 public:
 	cartesian() = default;
+	cartesian(const zaimoni::gdi::box<coord_type>& src) : _domain(src) {
+		_size = size(_domain);	// syntax-check: forces throw if not fully representable \todo something more legible
+	};
 	cartesian(FILE* src)
 	{
 		zaimoni::read(_domain.tl_c(), src);
 		zaimoni::read(_domain.br_c(), src);
+		_size = size(_domain);
 	}
 
 	ZAIMONI_DEFAULT_COPY_DESTROY_ASSIGN(cartesian);
@@ -39,6 +44,73 @@ public:
 	{
 		zaimoni::write(_domain.tl_c(), dest);
 		zaimoni::write(_domain.br_c(), dest);
+	}
+
+	// thin-forwarders
+	const zaimoni::gdi::box<coord_type>& domain() const { return _domain; }	// insecure
+	bool contains(const coord_type& src) const { return _domain.contains(src); }
+
+	// coordinates
+	static bool is_representable(const zaimoni::gdi::box<coord_type>& src) {
+		size_t ub = N;
+		size_t ret = 1;
+		size_t tolerate = (size_t)(-1);
+		do {
+			--ub;
+			const auto upper = src.br_c()[ub];
+			const auto lower = src.tl_c()[ub];
+			// \todo convert following to Augment.STL/typetraits wrapper
+			if (lower >= upper) return false;
+			const auto test = zaimoni::pos_diff(upper, lower);
+			if (test > tolerate) return false;
+			ret *= test;
+			tolerate /= test;
+		} while (0 < ub);
+		return true;
+	}
+	static size_t size(const zaimoni::gdi::box<coord_type>& src) {
+		size_t ub = N;
+		size_t ret = 1;
+		size_t tolerate = (size_t)(-1);
+		do {
+			--ub;
+			const auto upper = src.br_c()[ub];
+			const auto lower = src.tl_c()[ub];
+			if (lower >= upper) throw std::runtime_error("denormalized");
+			const auto test = zaimoni::pos_diff(upper, lower);
+			if (test > tolerate) throw std::runtime_error("coordinate range not representable");
+			ret *= test;
+			tolerate /= test;
+		} while (0 < ub);
+		return ret;
+	}
+	size_t size() const { return _size; }
+	size_t index(const coord_type& src) const {
+		size_t ub = N;
+		size_t ret = 0;
+		do {
+			--ub;
+			const auto upper = _domain.br_c()[ub];
+			const auto lower = _domain.tl_c()[ub];
+			const auto test_coord = src[ub];
+			if (lower > test_coord || upper <= test_coord) return (size_t)(-1);	// out of bounds
+			ret *= zaimoni::pos_diff(upper, lower);
+			ret += zaimoni::pos_diff(test_coord, lower);
+			if ((size_t)(-1) == ret) return (size_t)(-1);	// predicted to denormalize
+		} while (0 < ub);
+		return ret;
+	}
+	coord_type coords(size_t src) const {
+		coord_type ret(0);
+		size_t i = 0;
+		do {
+			const auto upper = _domain.br_c()[i];
+			const auto lower = _domain.tl_c()[i];
+			const auto test = zaimoni::pos_diff(upper, lower);
+			ret[i] = src % test;
+			src /= test;
+		} while (++i < N);
+		return ret;
 	}
 };
 
