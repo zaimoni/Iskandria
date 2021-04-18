84c79b41ec5e0ba734ced08c8f76781706109d7f
zaimoni <tharichizi@gmail.com>
2021-04-18 05:13:13 -0500

    checkpoint kuroda grammar-friendly variable support

--- Zaimoni.STL/var.hpp
+++ Zaimoni.STL/var.hpp
@@ -199,6 +199,146 @@ namespace bits {
 
 }	// namespace zaimoni
 
+// close/re-open to separate legacy implementation from experimental implementation
+
+// above is not Kuroda-grammar friendly.  Try to do what Franci does here
+namespace zaimoni {
+
+	namespace detail {
+
+		template<class T> struct var_fp_impl;
+
+		template<>
+		struct var_fp_impl<double> {
+			using param_type = double;
+
+			static const math::type* domain(const param_type& x)  {
+				if (std::isnan(x)) return nullptr;
+				if (std::isinf(x)) return &zaimoni::math::get<_type<_type_spec::_R_SHARP_>>();
+				return &zaimoni::math::get<_type<_type_spec::_R_>>();
+			}
+			static constexpr bool self_eval(const param_type& x) { return false; }
+			static constexpr int sgn(const param_type& x) { return 0 < x ? 1 : (0 > x ? -1 : 0); }
+			static std::string to_s(const param_type& x) { return std::to_string(x); }
+			static bool is_scal_bn_identity(const param_type& x) {
+				return _fp_stats<param_type>(x).is_scal_bn_identity();
+			}
+			static std::pair<intmax_t, intmax_t> scal_bn_safe_range(const param_type& x) {
+				std::pair<intmax_t, intmax_t> ret(fp_API::max_scal_bn_safe_range());
+				_fp_stats<param_type>(x).scal_bn_safe_range(ret.first, ret.second);
+				return ret;
+			}
+			static intmax_t ideal_scal_bn(const double& x) {
+				return _fp_stats<param_type>(x).ideal_scal_bn();
+			}
+			static fp_API* clone(const param_type& x) { return nullptr; }
+			static void _scal_bn(param_type& x, intmax_t scale) { x = std::scalbn(x, scale); }
+			static constexpr fp_API* _eval(const param_type& x) { return nullptr; }
+		};
+
+		template<>
+		struct var_fp_impl<float> {
+			using param_type = float;
+
+			static const math::type* domain(const param_type& x) {
+				if (std::isnan(x)) return nullptr;
+				if (std::isinf(x)) return &zaimoni::math::get<_type<_type_spec::_R_SHARP_>>();
+				return &zaimoni::math::get<_type<_type_spec::_R_>>();
+			}
+			static constexpr bool self_eval(const param_type& x) { return false; }
+			static constexpr int sgn(const param_type& x) { return 0 < x ? 1 : (0 > x ? -1 : 0); }
+			static std::string to_s(const param_type& x) { return std::to_string(x); }
+			static bool is_scal_bn_identity(const param_type& x) {
+				return _fp_stats<param_type>(x).is_scal_bn_identity();
+			}
+			static std::pair<intmax_t, intmax_t> scal_bn_safe_range(const param_type& x) {
+				std::pair<intmax_t, intmax_t> ret(fp_API::max_scal_bn_safe_range());
+				_fp_stats<param_type>(x).scal_bn_safe_range(ret.first, ret.second);
+				return ret;
+			}
+			static intmax_t ideal_scal_bn(const double& x) {
+				return _fp_stats<param_type>(x).ideal_scal_bn();
+			}
+			static fp_API* clone(const param_type& x) { return nullptr; }
+			static void _scal_bn(param_type& x, intmax_t scale) { x = std::scalbn(x, scale); }
+			static constexpr fp_API* _eval(const param_type& x) { return nullptr; }
+		};
+
+		template<>
+		struct var_fp_impl<long double> {
+			using param_type = long double;
+
+			static const math::type* domain(const param_type& x) {
+				if (std::isnan(x)) return nullptr;
+				if (std::isinf(x)) return &zaimoni::math::get<_type<_type_spec::_R_SHARP_>>();
+				return &zaimoni::math::get<_type<_type_spec::_R_>>();
+			}
+			static constexpr bool self_eval(const param_type& x) { return false; }
+			static constexpr int sgn(const param_type& x) { return 0 < x ? 1 : (0 > x ? -1 : 0); }
+			static std::string to_s(const param_type& x) { return std::to_string(x); }
+			static bool is_scal_bn_identity(const param_type& x) {
+				return _fp_stats<param_type>(x).is_scal_bn_identity();
+			}
+			static std::pair<intmax_t, intmax_t> scal_bn_safe_range(const param_type& x) {
+				std::pair<intmax_t, intmax_t> ret(fp_API::max_scal_bn_safe_range());
+				_fp_stats<param_type>(x).scal_bn_safe_range(ret.first, ret.second);
+				return ret;
+			}
+			static intmax_t ideal_scal_bn(const double& x) {
+				return _fp_stats<param_type>(x).ideal_scal_bn();
+			}
+			static fp_API* clone(const param_type& x) { return nullptr; }
+			static void _scal_bn(param_type& x, intmax_t scale) { x = std::scalbn(x, scale); }
+			static constexpr fp_API* _eval(const param_type& x) { return nullptr; }
+		};
+
+	}
+
+	template<class T>
+	class var_fp final : public fp_API // "variable, floating-point"
+	{
+	public:
+		T _x;	// we would provide full accessors anyway so may as well be public
+
+		var_fp() = default;
+		var_fp(const T& src) noexcept(std::is_nothrow_copy_constructible_v<T>) : _x(src) {}
+		var_fp(const var_fp& src) = default;
+		var_fp(var_fp&& src) = default;
+		virtual ~var_fp() = default;
+		var_fp& operator=(const var_fp& src) = default;
+		var_fp& operator=(var_fp&& src) = default;
+		var_fp& operator=(const T& src) noexcept(std::is_nothrow_copy_assignable_v<T>) {
+			_x = src;	// presumably this is ACID
+			return *this;
+		};
+
+		// placeholders
+		const math::type* domain() const override { return detail::var_fp_impl<T>::domain(_x); }
+		// implemented
+		bool self_eval() override { return detail::var_fp_impl<T>::self_eval(_x); }
+		bool is_zero() const override { return zaimoni::is_zero(_x); }
+		bool is_one() const override { return zaimoni::is_one(_x); }
+		int sgn() const override { return detail::var_fp_impl<T>::sgn(_x); }
+		// scalbn: scale by power of 2.  Important operation as it's infinite-precision (when it works)
+		bool is_scal_bn_identity() const override { return detail::var_fp_impl<T>::is_scal_bn_identity(_x); }
+		std::pair<intmax_t, intmax_t> scal_bn_safe_range() const override { return detail::var_fp_impl<T>::scal_bn_safe_range(_x); }
+		intmax_t ideal_scal_bn() const override { return detail::var_fp_impl<T>::ideal_scal_bn(_x); }
+
+		// technical infrastructure
+		fp_API* clone() const override {
+			if (fp_API* test = detail::var_fp_impl<T>::clone(_x)) return test;
+			return new var_fp(_x);
+		}
+		std::string to_s() const override { return detail::var_fp_impl<T>::to_s(_x); }
+		int precedence() const override { return std::numeric_limits<int>::max(); }	// things like numerals generally outrank all operators
+
+	private:
+		void _scal_bn(intmax_t scale) override { return detail::var_fp_impl<T>::_scal_bn(_x, scale); }	// power-of-two
+		fp_API* _eval() const override { return detail::var_fp_impl<T>::_eval(_x); }	// memory-allocating evaluation
+	};
+
+}
+
 #ifdef _INTERVAL_HPP
 #include "bits/_interval_var.hpp"
 #endif
