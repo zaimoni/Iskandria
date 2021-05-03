df0eb993d9b90f65a69c18f2ec7569a547a8456d
zaimoni <tharichizi@gmail.com>
2021-05-03 17:57:51 -0500

    checkpoint symbolic additive/multiplicative inverse

--- func.hpp
+++ func.hpp
@@ -1,8 +1,10 @@
-#ifndef ARY1_HPP
-#define ARY1_HPP
+#ifndef FUNC_HPP
+#define FUNC_HPP
 
 #include <vector>
 #include <functional>
+#include <tuple>
+#include "Zaimoni.STL/augment.STL/type_traits"
 #include "Zaimoni.STL/Logging.h"
 
 namespace zaimoni {
@@ -10,12 +12,20 @@ namespace math {
 
 template<class Range, class...Args>
 class func {
+public:
+	using InvRange = std::conditional_t<
+		1==sizeof...(Args),
+		nth_element<0, Args...>,
+		std::tuple<Args...> >;
+private:
 	unsigned long _code;
 	static std::vector<std::function<Range(Args...)>> _funcs;
+	static std::vector<std::function<InvRange(Range)>> _inv_funcs;
 
 public:
-	static auto declare(std::function<Range(Args...)> forward) {
+	static auto declare(std::function<Range(Args...)> forward, std::function<InvRange(Range)> inverse) {
 		_funcs.push_back(forward);
+		_inv_funcs.push_back(inverse);
 		return _funcs.size();
 	}
 
@@ -30,38 +40,11 @@ public:
 
 	auto code() const { return _code; }
 
-};
-
-template<class Domain, class Range>
-class ary1
-{
-	unsigned long _code;
-	static std::vector<std::function<Range(Domain)>> _funcs;
-	static std::vector<std::function<Domain(Range)>> _inv_funcs;
-
-public:
-	static auto declare(std::function<Range(Domain)> forward, std::function<Domain(Range)> inverse) {
-		_funcs.push_back(forward);
-		_inv_funcs.push_back(inverse);
-		return _funcs.size();
-	}
-
-	ary1(unsigned long code = 0) noexcept : _code(code) {
-		assert(_funcs.size() > code);
-	}
-	ary1(const ary1& src) = default;
-	ary1(ary1&& src) = default;
-	~ary1() = default;
-	ary1& operator=(const ary1& src) = default;
-	ary1& operator=(ary1 && src) = default;
-
-	auto code() const { return _code; }
-
 	bool can_eval() const { return _funcs[_code]; }
 	bool can_inv_eval() const { return _inv_funcs[_code]; }
 
-	Range eval(Domain src) const { return _funcs[_code](src); }
-	Domain inv_eval(Range src) const { return _inv_funcs[_code](src); }
+	Range eval(Args... src) const { return _funcs[_code](src); }
+	InvRange inv_eval(Range src) const { return _inv_funcs[_code](src); }
 };
 
 }
